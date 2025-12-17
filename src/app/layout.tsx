c4899860fce43ea6fdf1b9d7d1e05952cb023203
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-12-16 19:01:52 -0500

    initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import '../assets/globals.css'
+
+const roboto = Roboto({
+	variable: '--font-roboto',
+	subsets: ['latin'],
+	weight: ['400', '500', '700', '900'],
+	display: 'swap',
+})
+
+export const metadata: Metadata = {
+	title: 'Tabella',
+	description: 'Tabella created with next.js',
+	icons: [{ rel: 'icon', url: '/favicon.svg' }],
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html lang='en'>
+			<body
+				className={`${roboto.className} antialiased`}
+				suppressHydrationWarning
+			>
+				{children}
+			</body>
+		</html>
+	)
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+	return (
+		<main className='flex h-screen items-center justify-center font-medium text-2xl'>
+			tabella
+		</main>
+	)
+}
